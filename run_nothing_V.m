4757a86a93ffb8bcc01f60b0367dcbdb138c2311
Paula Desire Valdor <pdesirev@pcbe17376.dyndns.cern.ch>
2026-07-17 14:07:34 +0200

    Changes to these files

--- run_nothing_V.m
+++ run_nothing_V.m
@@ -1,13 +1,16 @@
+clc
+clear
 addpath('/home/pdesirev/rf-track-2.1')
 RF_Track;
-RF_Track.number_of_threads = 8;
+RF_Track.number_of_threads = 1;
 
 % Gaussian buch
 M = load('Results/initial_particles_V.dat');
 mass = RF_Track.protonmass;
 Q = -1; % antiprotons
 charge = 5e7;
-B0 = Bunch6dT(mass, charge, Q, M);
+%B0 = Bunch6dT(mass, charge, Q, M);
+clear M
 
 P_ref = 13.7; % MeV/c
 mass = RF_Track.protonmass;
@@ -15,20 +18,56 @@ K = 0.1;
 E = K + mass;
 beta_rel = P_ref/E;
 
-L = Lattice("elena.tws"); % To be generated with the notebook. Adapt the magnitudes
-h = 4 % up to 4 bunches
-length = L.get_length();
-c = 299792458;
-f_rf = h * c * beta_rel / length;
+B0 = Bunch6dT(RF_Track.protonmass, 0.0, -1, [ 0 0 0 0 0 P_ref ]);
+%L = Lattice("elena.tws"); % To be generated with the notebook. Adapt the magnitudes
 
 V = Volume();
 V.odeint_algorithm = 'analytic';
-V.add(L, 0.0, 0.0, 0.0, reference='entrance');
+%V.add(L, 0.0, 0.0, 0.0, reference='entrance');
+Bx_struct = load('Bx.dat');
+Bx = Bx_struct.Bx;
+disp('after Bx')
+clear Bx_struct
+By_struct = load('By.dat');
+By = By_struct.By;
+clear By_struct
+disp('after By')
+Bz_struct = load('Bz.dat');
+Bz = Bz_struct.Bz;
+clear Bz_struct 
+disp('after Bz')
+
+xa = load('xa.dat','-ascii');
+ya = load('ya.dat','-ascii');
+za = load('za.dat','-ascii');
+x0 = xa(1);
+y0 = ya(1);
+z0 = za(1);
+hx = xa(2)-xa(1);
+hy = ya(2)-ya(1);
+hz = za(2)-za(1);
+Lmap = za(end)-za(1);
+clear za
+disp("before")
+system("ps -o pid,rss,vsz,cmd -C octave-gui")
+Bfield = Static_Magnetic_FieldMap(...
+    Bx, By, Bz,...
+    x0, y0,...
+    hx, hy, hz,
+    Lmap);
+disp("after")
+system("ps -o pid,rss,vsz,cmd -C octave-gui")
+
+disp('after Static_Magnetic_FieldMap')
+V.add(Bfield, 0.0, 0.0, z0, 'entrance');
+
 V.verbosity = 1;
-V.t_max_mm = L.get_length() / beta_rel *1e3 * 100;
-V.dt_mm = 300;
-V.tt_dt_mm = 10000;
+V.t_max_mm = 30.405 / beta_rel *1e3 * 50;
+V.dt_mm = 100;
+V.tt_dt_mm = 3000;
+%V.wp_dt_mm = V.get_length() * 10/ beta_rel *1e3;
+%V.wp_basename = 'Results/ps_nothing'
 B1 = V.track(B0);
-T = V.get_transport_table("%mean_Z %emitt_x %emitt_y %sigma_Z %N");
+T = V.get_transport_table("%mean_Z %mean_X %mean_Y %emitt_x %emitt_y %sigma_Z %N");
 save -text Results/transport_table_nothing.dat T
 
--- run_volume_tracking.m
+++ run_volume_tracking.m
@@ -1,3 +1,5 @@
+clc
+clear
 RF_Track;
 
 P_ref = 13.7; % MeV/c
@@ -8,8 +10,8 @@ beta_rel = P_ref/E;
 
 %% Load lattice
 RCS = Lattice('elena.tws');
-%RCS.set_width(1.0);
-%RCS.set_height(0.1);
+RCS.set_width(1.0);
+RCS.set_height(0.1);
 
 %% Place the lattice in Volume for time integration
 V = Volume();
@@ -18,38 +20,49 @@ V.dt_mm = 10;
 V.set_s0 = 0.0;
 %V.sc_dt_mm = 400;
 V.tt_dt_mm = 1000;
-V.t_max_mm = RCS.get_length() * 1e3 * 5/ beta_rel; % about 10 turns
+V.t_max_mm = RCS.get_length() * 1e3 *2 / beta_rel; % about 10 turns
 
-%% Plot the field
-xa = linspace(-10,  10, 1501); % m
-za = linspace(-10, 10, 1500); % m
-[X,Z] = ndgrid(xa*1e3,za*1e3); % mm
-[E,B] = V.get_field (X(:), 0, Z(:), 0);
+xa = linspace(-9,1,2000);   % x [m]
+ya = linspace(-0.05,0.05,4); % y [m]
+za = linspace(-6,6,3000);     % z [m]
+
+[X,Y,Z] = ndgrid(xa*1e3, ya*1e3, za*1e3); % mm
+
+[E,B] = V.get_field(X(:),Y(:),Z(:),0);
+
 B(isnan(B)) = 0;
 
-By = reshape(B(:,2), size(X)); % T
+Bx = reshape(B(:,1),size(X));
+By = reshape(B(:,2),size(X));
+Bz = reshape(B(:,3),size(X));
+save -text Bx.dat Bx
+save -text By.dat By
+save -text Bz.dat Bz
+save -text xa.dat xa
+save -text ya.dat ya
+save -text za.dat za
 
 %figure(1)
 %clf
 %pcolor(za, xa, By)
 %xlim([ min(za), max(za) ])
 %ylim([ min(xa), max(xa) ])
-%shading flat
+
 %xlabel('Z [m]')
 %ylabel('X [m]')
 %daspect([ 1 1 ])
 %shading flat
 
 %%  Define a Bunch
-M = load('Results/initial_particles_V.dat');
-Q = -1; % antiprotons
-charge = 1e7;
-B0 = Bunch6dT(mass, charge, Q, M);
+%M = load('Results/initial_particles_V.dat');
+%Q = -1; % antiprotons
+%charge = 1e7;
+%B0 = Bunch6dT(mass, charge, Q, M);
 
 
-V.verbosity=2
-B1 = V.track(B0);
-T = V.get_transport_table('%mean_Z %mean_X %mean_P %emitt_x %emitt_y %emitt_4d %sigma_Z') / 1e3; % m
-save -text transport_table.dat T
-L = V.get_lost_particles();
-save -text lost_particles.dat L
+%V.verbosity=2
+%B1 = V.track(B0);
+%T = V.get_transport_table('%mean_Z %mean_X %mean_P %emitt_x %emitt_y %emitt_4d %sigma_Z') / 1e3; % m
+%save -text transport_table.dat T
+%L = V.get_lost_particles();
+%save -text lost_particles.dat L
